afa3e6bfd4610647c3e0fab077e541eb2894dd57
Trond Trosterud <trond.trosterud@uit.no>
2022-08-29 08:57:12 +0200

    hfst, not xfst

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -11,6 +11,7 @@
 
 
 LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
 GTLANGS=$(echo $GTLANGS)
 
 
@@ -25,7 +26,8 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-olo/src/generator-gt-norm.xfst
+#  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-olo/src/generator-gt-norm.xfst
+  echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-olo/src/generator-gt-norm.hfst
  done
  rm -f $L_FILE
 done
