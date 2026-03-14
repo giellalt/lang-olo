b5e8c8c4daa1b5ac42daaa6cb396fd73dc9f4018
Trondtr <trond.trosterud@uit.no>
2026-03-14 15:31:07 +0100

    path

--- devtools/numerals_minip.sh
+++ devtools/numerals_minip.sh
@@ -25,7 +25,7 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-  echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-olo/src/generator-gt-norm.hfstol
+  echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-olo/src/fst/generator-gt-norm.hfstol
  done
  rm -f $L_FILE
 done
